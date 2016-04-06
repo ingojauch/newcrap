879d0ce57ef8b879ac606b4c636c2e4437e2527d
Gustavo Maestri <gaa@certi.org.br>
2016-04-06 09:54:51 -0300

    fix typo

--- lib/rubycrap/application.rb
+++ lib/rubycrap/application.rb
@@ -3,111 +3,50 @@ require 'flog_cli'
 require 'json'
 require 'builder'
 require 'rubycrap/logging'
+require 'rubycrap/coverage'
 
 module Rubycrap
-class Application 
-	@simplecov_information=[]
-	@crap_methods=[]
-	def self.minfo(object)
-	  puts ">supports: #{(object.methods  - Object.methods).inspect}\n"
-	end
+  class Application
+    @simplecov_information=[]
+    @crap_methods=[]
 
- # def self.Rubycrap::logger
-  #  @Rubycrap::logger ||= Logger.new(STDOUT)
- # end
-	#
-	# => reads the sourcefile with an ast parser to get all the methods, then calculate the method coverage
-	#
-	def self.process_simplecov_file(file)
-	  #get filename with its coverage information
-	  filename = file["filename"]
-    Rubycrap::logger.debug(filename)
-	  parse_method_coverage(file,filename)
-	end
+    def self.minfo(object)
+      puts ">supports: #{(object.methods  - Object.methods).inspect}\n"
+    end
 
-	def self.parse_method_coverage(file,filename)
-	  ast = Parser::CurrentRuby.parse(File.open(filename, "r").read)
-	  recursive_search_ast(file,ast)
-	end
+    def self.crap(score,coverage)
+#    CRAP(m) = comp(m)^2 * (1 � cov(m)/100)^3 + comp(m)
+      comp = score
+      cov = coverage
+      comp ** 2 * (1 - cov) ** 3 + comp
+    end
 
-	def self.calculate_method_coverage(file,startline,lastline)
-	  # first we get the coverage lines from simplecov
-	  # start position -1 and number of total lines (-1 if you dont want the end)
-	  total_lines = lastline-startline
-    Rubycrap::logger.debug("startline #{startline}")
-    Rubycrap::logger.debug("latline #{lastline}")
-	  coveragelinestotal = file["coverage"]
-	  	 
-    Rubycrap::logger.debug( "total_lines #{total_lines}")
-	  coveragelines = coveragelinestotal.slice(startline-1,total_lines)
-    Rubycrap::logger.debug("coveragelines: #{coveragelines}")
-	  covered_lines = 0
-	  coveragelines.each do |line|
-	    if !(line.to_s.eql? "0" or line.to_s.eql? "")
-	      covered_lines = covered_lines + 1
-	    end
-	  end
-	  method_coverage = covered_lines.to_f / total_lines.to_f
-    Rubycrap::logger.debug("method_coverage: #{method_coverage}")
-	  return method_coverage
-	end
+    def self.calcualte_crap_from_flog(file)
+      begin
+        FlogCLI.load_plugins
+        options = FlogCLI.parse_options "-qma"
+        flogger = FlogCLI.new options
 
-	def self.recursive_search_ast(file,ast)
-		begin
-			ast.children.each do |child|
-				if child.class.to_s == "Parser::AST::Node"
-				  if (child.type.to_s == "def" or child.type.to_s == "defs")
-
-				    methodname = child.children[0].to_s
-				    startline = child.loc.line
-				    lastline = child.loc.last_line
-				    Rubycrap::logger.debug("\nmethodname: #{methodname}")
-				    method_coverage = calculate_method_coverage(file,startline,lastline)
-
-				    @simplecov_information << {:name => methodname, :coverage => method_coverage , :startline => startline, :lastline => lastline}
-				  else
-				    recursive_search_ast(file,child)
-				  end
-				end
-			end
-		rescue
-			# maybe empty source code
-		end
-	end
-
-	def self.crap(score,coverage)
-	#    CRAP(m) = comp(m)^2 * (1 � cov(m)/100)^3 + comp(m)
-	  comp = score
-	  cov = coverage
-	  comp ** 2 * (1 - cov) ** 3 + comp
-	end
-
-	def self.calcualte_crap_from_flog(file)
-		begin
-			FlogCLI.load_plugins
-			options = FlogCLI.parse_options "-qma"
-			fRubycrap::logger = FlogCLI.new options
-
-			fRubycrap::logger.flog file["filename"]
-      Rubycrap::logger.debug("fRubycrap::logger absolute_filename: #{file["filename"]}")
-			fRubycrap::logger.each_by_score nil do |class_method, score, call_list|
-				startline = fRubycrap::logger.method_locations[class_method].split(":")[1]
-				absolute_filename = fRubycrap::logger.method_locations[class_method].split(":")[0]
-        Rubycrap::logger.debug("fRubycrap::logger startline: #{startline}")
-				#match simplecov line with startine form floc
-				element = @simplecov_information.detect {|f| f[:startline] == startline.to_i }
-				if element.to_s == ""
-          Rubycrap::logger.debug("no match with simplecov for Rubycrap::logger class_method: #{class_method} startline: #{startline} ")
-				else
-          Rubycrap::logger.debug("fRubycrap::logger class_method: #{class_method} simplecov: #{element}")
-					test_coverage = element[:coverage]
-					@crap_methods << {:methodname => class_method, :flog_score => score , :filepath => absolute_filename, :startline => startline, :method_coverage => test_coverage, :crap_score => crap(score,test_coverage)}
-				end
-			end
-		rescue
-			# something went wrong with flog
-		end
-	end
+        flogger.flog file["filename"]
+        Rubycrap::logger.debug("flogger absolute_filename: #{file["filename"]}")
+        flogger.each_by_score nil do |class_method, score, call_list|
+          startline = flogger.method_locations[class_method].split(":")[1]
+          absolute_filename = flogger.method_locations[class_method].split(":")[0]
+          Rubycrap::logger.debug("flogger startline: #{startline}")
+#match simplecov line with startine form floc
+          element = @simplecov_information.detect {|f| f[:startline] == startline.to_i }
+          if element.to_s == ""
+            Rubycrap::logger.debug("no match with simplecov for logger class_method: #{class_method} startline: #{startline} ")
+          else
+            Rubycrap::logger.debug("fRubycrap::logger class_method: #{class_method} simplecov: #{element}")
+            test_coverage = element[:coverage]
+            @crap_methods << {:methodname => class_method, :flog_score => score , :filepath => absolute_filename, :startline => startline, :method_coverage => test_coverage, :crap_score => crap(score,test_coverage)}
+          end
+        end
+      rescue
+        # something went wrong with flog
+      end
+    end
 
 	def self.hasharray_to_html( hashArray )
 	  # collect all hash keys, even if they don't appear in each hash:
@@ -123,27 +62,22 @@ class Application
 	  return html
 	end
 
-	def self.run(coveragefile,mode)
+  def self.run(coveragefile,mode)
     Rubycrap::logger.level = mode
     coverage = JSON.parse(File.open(coveragefile, "r").read)
-
-		# file = coverage["files"].first
-		#
-		# => get all the source filenames from the coverage file
-		#
-		puts "total files: #{coverage["files"].count}"
-		coverage["files"].each.with_index(1) do |file, index|
+    puts "total files: #{coverage["files"].count}"
+    coverage["files"].each.with_index(1) do |file, index|
       Rubycrap::logger.debug("file nr. #{index}")
-		  @simplecov_information = Rubycrap::Coverage.new(file).process_simplecov_file
-		  calcualte_crap_from_flog(file)
-		end
+      @simplecov_information = Rubycrap::Coverage.new(file).process_simplecov_file
+      calcualte_crap_from_flog(file)
+    end
 
-		@sorted = @crap_methods.sort_by { |k| -k[:crap_score] }
+    @sorted = @crap_methods.sort_by { |k| -k[:crap_score] }
 
-		puts "\nRESULTS"
-		@sorted.each do |element|
-			puts "#{element[:crap_score].round}".ljust(6) + "#{element[:methodname]}  ---> #{element[:filepath]}:#{element[:startline]}"
-		end
+    puts "\nRESULTS"
+    @sorted.each do |element|
+      puts "#{element[:crap_score].round}".ljust(6) + "#{element[:methodname]}  ---> #{element[:filepath]}:#{element[:startline]}"
+    end
 
 		#
 		# buidler sucks
@@ -162,9 +96,9 @@ class Application
 
 		# <table id="myTable">
 
-		File.open("CRAP.html", 'w') { |file| file.write(hasharray_to_html(@sorted)) }
-		puts "THE END"
-	end
+    File.open("CRAP.html", 'w') { |file| file.write(hasharray_to_html(@sorted)) }
+    puts "THE END"
+  end
 
 end
 end
